76a39726f91b603f8784daf69de9a0a0aa716fc7
FuadMannan <fuad.mannan@gmail.com>
2017-07-27 11:59:41 -0400

    Add project files.

---
+++ COMP123-S2017-Lesson11A/CardList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 27, 2017
+ * Description: This is the abstract superclass CardList.
+ * It inherits from the build-in List generic collection and consumes the Card class
+ * Version: 0.1 - Created the CardList class
+ */
+
+namespace COMP123_S2017_Lesson11A
+{
+    public abstract class CardList: List<Card>
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+        public CardList()
+        {
+            this._initialize();
+        }
+
+        // PRIVATE METHODS
+        protected abstract void _initialize();
+
+        // PUBLIC METHODS
+    }
+}
